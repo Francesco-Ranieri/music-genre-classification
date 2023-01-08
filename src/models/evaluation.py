aeb7785e2413c445df93ad23726693f1d4719fd1
Francesco Ranieri <ranierifra99@hotmail.it>
2023-01-08 23:27:48 +0100

    feat: add train model script

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -1,72 +1,91 @@
-# Utilities to run evaluation of classifiers
-
-
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import KFold
 from sklearn import metrics
+import numpy as np
 
 
-# We can perform both KFold validation and hold-out validation, but because of computational cost only the latter is
-# used.
 def evaluate_classifier(classifier,
                         n_folds,
-                        X_train_split,
+                        x_train_split,
                         y_train_split,
-                        X_validation_split=None,
+                        x_validation_split=None,
                         y_validation_split=None,
                         fit=True):
     """
     Utilities to run evaluation of classifiers
-    The splits are not performed in these methods because validation sets need to be stored in order to have a reliable evaluation of
-    models that are stored and then reloaded.
 
-    If n_folds is falsy, perform a validation with a single test/validation split, using the other parameters
-
+    If n_folds is false, perform a validation with a single test/validation split, using the other parameters
+    :param classifier: model used to classify
+    :param n_folds: number of folds in cross validation
+    :param x_train_split: X train set
+    :param y_train_split: y train set
+    :param x_validation_split: X test set
+    :param y_validation_split: y test set
+    :param fit: indicates if the model needs to be fit or not
+    :return:
     """
 
     if n_folds:
-        accuracy, precision, recall, fmacro, fmicro = _evaluate_classifier_with_folds(classifier, fit, n_folds)
+        accuracy, precision, recall, f_macro, f_micro, classifier = _evaluate_classifier_with_folds(classifier,
+                                                                                                    fit,
+                                                                                                    x_train_split,
+                                                                                                    y_train_split,
+                                                                                                    n_folds)
     else:
-        accuracy, precision, recall, fmacro, fmicro = _evaluate_classifier_with_split(classifier, fit, X_train_split,
-                                                                                      X_validation_split, y_train_split,
-                                                                                      y_validation_split)
-
+        accuracy, precision, recall, f_macro, f_micro, classifier = _evaluate_classifier_with_split(classifier,
+                                                                                                    fit,
+                                                                                                    x_train_split,
+                                                                                                    x_validation_split,
+                                                                                                    y_train_split,
+                                                                                                    y_validation_split)
     print("Accuracy: ", accuracy)
     print("Precision: ", precision)
     print("Recall: ", recall)
-    print("F1-micro: ", fmicro)
-    print("F1-macro: ", fmacro)
+    print("F1-micro: ", f_micro)
+    print("F1-macro: ", f_macro)
 
-    return accuracy, precision, recall, fmacro, fmicro
+    metrics_calc = {
+                        "accuracy": accuracy,
+                        "precision": precision,
+                        "recall": recall,
+                        "f_micro": f_micro,
+                        "f_macro": f_macro
+                    }
+
+    return metrics_calc, classifier
 
 
-def _evaluate_classifier_with_folds(classifier, fit, n_folds):
-    accuracy, precision, recall, fmacro, fmicro = 0, 0, 0, 0, 0
-    X_train_array = np.array(X_train)
+def _evaluate_classifier_with_folds(classifier,
+                                    fit,
+                                    n_folds,
+                                    X_train,
+                                    y_train):
+    accuracy, precision, recall, f_macro, f_micro = 0, 0, 0, 0, 0
+    x_train_array = np.array(X_train)
 
     k_fold = KFold(n_splits=n_folds, random_state=0, shuffle=True)
-    for idx, (train_index, validation_index) in enumerate(k_fold.split(X_train_array)):
+    for idx, (train_index, validation_index) in enumerate(k_fold.split(x_train_array)):
 
         print(f"Fold #{idx}")
         print(f"Fitting the model...")
 
         if fit:
-            classifier.fit(X_train_array[train_index], y_train[train_index])
+            classifier.fit(x_train_array[train_index], y_train[train_index])
 
         print(f"Model fitted. Predicting on validation set...")
-        predicted = classifier.predict(X_train_array[validation_index])
+        predicted = classifier.predict(x_train_array[validation_index])
         accuracy = accuracy + metrics.accuracy_score(y_train[validation_index], predicted)
         precision = precision + metrics.precision_score(y_train[validation_index], predicted, average='micro')
         recall = recall + metrics.recall_score(y_train[validation_index], predicted, average='micro')
-        fmicro = fmicro + metrics.f1_score(y_train[validation_index], predicted, average='micro')
-        fmacro = fmacro + metrics.f1_score(y_train[validation_index], predicted, average='macro')
+        f_micro = f_micro + metrics.f1_score(y_train[validation_index], predicted, average='micro')
+        f_macro = f_macro + metrics.f1_score(y_train[validation_index], predicted, average='macro')
 
     accuracy /= n_folds
     precision /= n_folds
     recall /= n_folds
-    fmicro /= n_folds
-    fmacro /= n_folds
+    f_micro /= n_folds
+    f_macro /= n_folds
 
-    return accuracy, precision, recall, fmacro, fmicro
+    return accuracy, precision, recall, f_macro, f_micro, classifier
 
 
 def _evaluate_classifier_with_split(classifier,
@@ -85,7 +104,7 @@ def _evaluate_classifier_with_split(classifier,
     accuracy = metrics.accuracy_score(y_validation_split, predicted)
     precision = metrics.precision_score(y_validation_split, predicted, average='macro')
     recall = metrics.recall_score(y_validation_split, predicted, average='macro')
-    fmicro = metrics.f1_score(y_validation_split, predicted, average='micro')
-    fmacro = metrics.f1_score(y_validation_split, predicted, average='macro')
+    f_micro = metrics.f1_score(y_validation_split, predicted, average='micro')
+    f_macro = metrics.f1_score(y_validation_split, predicted, average='macro')
 
-    return accuracy, precision, recall, fmacro, fmicro
+    return accuracy, precision, recall, f_macro, f_micro, classifier
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,1 +1,55 @@
+from sklearn.naive_bayes import GaussianNB
+from src.data.data_utils import get_dataset
+from src.models.evaluation import evaluate_classifier
+import mlflow
+import dotenv
 
+
+class ModelTrainer:
+    X_train = {}
+    y_train = {}
+    X_test = {}
+    y_test = {}
+
+    def __init__(self):
+        data = get_dataset()
+        self.X_train = data["X_train"]
+        self.y_train = data["y_train"]
+
+        self.X_train_split = data["X_train_split"]
+        self.y_train_split = data["y_train_split"]
+
+        self.X_test = data["X_test"]
+        self.y_test = data["y_test"]
+
+        self.X_validation = data["X_validation"]
+        self.y_validation = data["y_validation"]
+
+    def train_model(self, model, model_name: str = "model"):
+        """
+        :param model:
+        :param model_name:
+        :return:
+        """
+
+        mlflow.autolog()
+        metrics, trained_model = evaluate_classifier(model,
+                                                     0,
+                                                     self.X_train,
+                                                     self.y_train,
+                                                     self.X_test,
+                                                     self.y_test,
+                                                     fit=True)
+
+        mlflow.sklearn.log_model(sk_model=trained_model,
+                                 artifact_path="sklearn-model",
+                                 registered_model_name=model_name
+                                 )
+
+
+dotenv.load_dotenv(override=True)
+experiment_name = "GAUSSIAN_NAIVE_BAYES"
+with mlflow.start_run(run_name=experiment_name) as run:
+    classifier = GaussianNB()
+    model_trainer = ModelTrainer()
+    model_trainer.train_model(classifier, experiment_name)
