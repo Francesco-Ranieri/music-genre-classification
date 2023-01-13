178de0103976ad64d426fbf110ae04b6c7c01597
Francesco Ranieri <ranierifra99@hotmail.it>
2023-01-13 19:33:02 +0100

    feat: split training phase in dvc pipeline

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,12 +1,21 @@
+import argparse
+
 import dotenv
+
+from src.data.data_utils import Dataset
 from src.models.classes.gtzan_model import GtzanModel
 from src.models.classes.mfcc_model import MfccModel
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", "-d", help="Set training flow based on dataset")
+args = parser.parse_args()
+
+if args.dataset == Dataset.GTZAN.value:
+    model = GtzanModel()
+elif args.dataset == Dataset.MFCC.value:
+    model = MfccModel()
+else:
+    raise SystemError("Model not found")
+
 dotenv.load_dotenv(override=True)
-
-# gtza_trainer = GtzanModel()
-# gtza_trainer.train()
-
-mfcc_trainer = MfccModel()
-mfcc_trainer.train()
-
+model.train()
