30611db7caa5cf526fdc156dd5ee74d0c4e12f9f
Francesco Ranieri <ranierifra99@hotmail.it>
2023-01-16 00:23:34 +0100

    feat: add visualization utils for label mapping

---
+++ src/visualization/visualization_utils.py
@@ -0,0 +1,14 @@
+label = ['blues',
+         'classical',
+         'country',
+         'disco',
+         'hip hop',
+         'jazz',
+         'metal',
+         'pop',
+         'reggae',
+         'rock']
+
+
+def get_human_readable_label(label_number: int):
+    return label[label_number]
