cc55a468f718e208cbdd445fc3ee4f7cfeb3d371
Francesco Ranieri <ranierifra99@hotmail.it>
2023-01-19 00:39:43 +0100

    fix: autopep8 reformat code

--- src/api/entities/predict_model_request.py
+++ src/api/entities/predict_model_request.py
@@ -3,6 +3,7 @@ from typing import List
 from pydantic import BaseModel
 from src.api.entities.model_allowed_enum import ModelAllowed
 
+
 class PredictModelRequest(BaseModel):
     audio_array: List = []
     gtzan_model: ModelAllowed = ModelAllowed.RANDOM_FOREST
