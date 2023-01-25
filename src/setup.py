47382de46aab7b91195759a069b2baec611a9af0
Francesco Ranieri <ranierifra99@hotmail.it>
2023-01-25 17:26:29 +0100

    FIX: pypi pipeline

--- src/setup.py
+++ src/setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(
     name='feat_extractor',
     packages=["feat_extractor"],
-    version='0.0.5',
-    description='Gtzan feature Extractor!',
-    long_description='Gtzan feature Extractor!',
+    version='0.0.6',
+    description='Gtzan feature Extractor',
+    long_description='Gtzan feature Extractor',
+    long_description_content_type="text/markdown",
     author='ranierifr',
-    license='MIT'
+    license='MIT',
+    python_requires=">=3.6"
 )
